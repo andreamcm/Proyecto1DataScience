89c3eb32b8bdde94d737bca81681ff25ff2bfa5b
Michelle Bloomfield <blo16803@uvg.edu.gt>
2019-08-05 11:28:22 -0600

    se agrego la eliminacion de datos repetidos

--- limpiezaDatos.R
+++ limpiezaDatos.R
@@ -1,7 +1,16 @@
 install.packages("reshape")
 library(reshape)
 
-setwd("C:/Users/user/Documents/2019/UVG/Segundo Semestre/DataScience/Proyectos/Proyecto1/Proyecto1DataScience/DatosCrudos")
+install.packages("dplyr")
+library(dplyr)
+
+setwd("C:\\Users\\User\\Desktop\\Data Science\\Proyecto1DataScience\\DatosCrudos")
 temp = list.files(pattern="*.csv")
 myfiles = lapply(temp, read.csv)
 data <- merge_recurse(myfiles)
+
+options(max.print= 1000000000)
+unique(data)
+
+duplicated(data)
+x <-data %>% distinct()
